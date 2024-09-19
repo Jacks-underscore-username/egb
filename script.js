168b4f5f547c9cc1ba2f404edbbcd56c5e619d81
Jacks-underscore-username <jacks.underscore.username@gmail.com>
2024-09-19 14:21:02 -0700

    d0756943a4f9953d

--- script.js
+++ script.js
@@ -72,14 +72,14 @@ const os = require('os')
             ' 1. Run ```node script``` in a terminal opened to said folder. (note: the first two steps can be skipped by running one of these os specific commands in a terminal opened to the folder:)',
             '',
             '#### Shortcuts (run these commands to do the above steps automatically):',
-            ' * Windows: ```Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Jacks-underscore-username/egb/main/script.js" -OutFile "script.js" && node script```',
+            ' * Windows: ```Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Jacks-underscore-username/egb/main/script.js" -OutFile "script.js"; node script```',
             ' * Linux or Android: ```curl -O https://raw.githubusercontent.com/Jacks-underscore-username/egb/main/script.js && chmod +x script.js && node script```',
             ' 1. Edit the ```user```, ```repo```, and ```passphrase``` values in the auto generated ```config.json``` file.',
             '',
             'Thats it! You are now ready to use the EGB system. ',
             '',
             '### Usage',
-            'Just run ```egb save``` or ```egb load``` in any terminal.',
+            'Just run ```egb save``` or ```egb load``` in any terminal after you reload.',
             '',
             '***Created by [Jacks-underscore-username](https://github.com/Jacks-underscore-username), read more at [my blog.](https://jacks-project-hub.vercel.app/posts/Github%20backup/)***'].join('\n'), 'utf8')
         console.log('Config file generated, finish setup by filling it out, then run egb save / egb load to use.')
